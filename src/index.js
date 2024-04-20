6c8449e7583e2be15518abedbb3f2b7debedbd53
tonniegeetz <tonniegeetz@gmail.com>
2024-04-20 10:10:53 +0300

    Practice on dynamic fetch requests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 const init = () => {
-  
-}
+  //Accessing the value via the event object
+
+  // const inputForm = document.querySelector('form');
+  // inputForm.addEventListener('submit', (e) => {
+  //   e.preventDefault();
+  //   e.target.children[1].value;   //Accessing the value via the event object
+
+  // });
+
+  //Accessing input value directly
+
+  const inputForm = document.querySelector('form');
+
+  inputForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const input = document.querySelector('input#searchByID');
+    const inputValue = input.value;
+    console.log(inputValue);
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+  });
+};
 
 document.addEventListener('DOMContentLoaded', init);
